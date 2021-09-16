86bd540f00a353eed03f9906f7f6e1276805e130
Matthew <89292849+AviationRam@users.noreply.github.com>
2021-09-16 15:00:28 +1000

    Create rateLimit.js

---
+++ events/client/rateLimit.js
@@ -0,0 +1,13 @@
+//here the event starts
+module.exports = (client, rateLimitData) => {
+    console.log(JSON.stringify(rateLimitData).grey.italic.dim);
+}
+/**
+  * @INFO
+  * Bot Coded by ramdoguy3659#6613 | https://discord.gg/dCTW3qqACb
+  * @INFO
+  * Work for Rammy Bot | https://www.rammydashboard.tk/
+  * @INFO
+  * Please mention him / Rammy Bot, when using this Code!
+  * @INFO
+*/
