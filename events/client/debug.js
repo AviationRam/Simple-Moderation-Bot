aac46dc06b585042ffe12374f85bd1ded2269dc0
Matthew <89292849+AviationRam@users.noreply.github.com>
2021-09-16 14:57:01 +1000

    Create debug.js

---
+++ events/client/debug.js
@@ -0,0 +1,13 @@
+//here the event starts
+module.exports = (client, info) => {
+  //console.log(String(info).grey);
+}
+/**
+  * @INFO
+  * Bot Coded by ramdoguy3659#6613 | https://discord.gg/dCTW3qqACb
+  * @INFO
+  * Work for Rammy Bot | https://www.rammydashboard.tk/
+  * @INFO
+  * Please mention him / Rammy Bot, when using this Code!
+  * @INFO
+*/
